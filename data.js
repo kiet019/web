1129517a8ad1047a52039d111ba132968ff18b13
kiet <kietly019@gmail.com>
2023-06-06 23:30:24 +0700

    update

--- data.js
+++ data.js
@@ -1,118 +1,132 @@
-export const setup = {
-    bannerTitle : "Banner title here",
-    containerContent: "Go to your favorite websites, choose the product you love and checkout with us!",
-    page: [
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-        {
-            index: 1,
-            name: "Popular"
-        },
-    ],
-    optionsList: [
-        { value: "", text: "Select country" },
-        { value: "option2", text: "Option 2" },
-        { value: "option3", text: "Option 3" },
-        { value: "option4", text: "Option 4" }
-    ],
-    stores: [
-        {
-          saleText: "UP TO 75% OFF",
-          logoSrc: "/images/logo.jpg",
-          imgSrc: "/images/banner.jpg",
-          infoText: "Store 1"
-        },
-        {
-          saleText: "UP TO 50% OFF",
-          logoSrc: "/images/logo.jpg",
-          imgSrc: "/images/banner.jpg",
-          infoText: "Store 2"
-        },
-        {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-          {
-            saleText: "UP TO 75% OFF",
-            logoSrc: "/images/logo.jpg",
-            imgSrc: "/images/banner.jpg",
-            infoText: "Store 1"
-          },
-      ]
-}
+var setup = {
+  bannerTitle: "Banner title here",
+  containerContent:
+    "Go to your favorite websites, choose the product you love and checkout with us!",
+  page: [
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+    {
+      index: 1,
+      name: "Popular",
+    },
+  ],
+  optionsList: [
+    { value: "", text: "Select country" },
+    { value: "option2", text: "Option 2" },
+    { value: "option3", text: "Option 3" },
+    { value: "option4", text: "Option 4" },
+  ],
+  stores: [
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "logo.jpg",
+      imgSrc: "banner.jpg",
+      infoText: "Store 1",
+      href: "https://www.google.com/"
+    },
+  ],
+};
+window.setup = setup;
