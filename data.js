4f46099f72f20e0881d5d77f1cdeeee89d8d6d95
kiet <kietly019@gmail.com>
2023-06-06 22:19:45 +0700

    update

---
+++ data.js
@@ -0,0 +1,118 @@
+export const setup = {
+    bannerTitle : "Banner title here",
+    containerContent: "Go to your favorite websites, choose the product you love and checkout with us!",
+    page: [
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+        {
+            index: 1,
+            name: "Popular"
+        },
+    ],
+    optionsList: [
+        { value: "", text: "Select country" },
+        { value: "option2", text: "Option 2" },
+        { value: "option3", text: "Option 3" },
+        { value: "option4", text: "Option 4" }
+    ],
+    stores: [
+        {
+          saleText: "UP TO 75% OFF",
+          logoSrc: "/images/logo.jpg",
+          imgSrc: "/images/banner.jpg",
+          infoText: "Store 1"
+        },
+        {
+          saleText: "UP TO 50% OFF",
+          logoSrc: "/images/logo.jpg",
+          imgSrc: "/images/banner.jpg",
+          infoText: "Store 2"
+        },
+        {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+          {
+            saleText: "UP TO 75% OFF",
+            logoSrc: "/images/logo.jpg",
+            imgSrc: "/images/banner.jpg",
+            infoText: "Store 1"
+          },
+      ]
+}
