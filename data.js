a4d5a98b1d2c0b9bfd184ba8e3a611a131fda3e6
kiet019 <kietly019@gmail.com>
2023-06-09 11:27:01 +0700

    update

--- data.js
+++ data.js
@@ -45,93 +45,265 @@ var setup = {
   stores: [
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 15",
+      logoSrc: "amazon-logo.jpg",
+      imgSrc: "amazon-background.jpg",
+      infoText: "Amazon",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 15",
+      logoSrc: "ebay-logo.jpg",
+      imgSrc: "ebay-background.jpg",
+      infoText: "Ebay",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "walmat-logo.jpg",
+      imgSrc: "walmat-background.jpg",
+      infoText: "Walmat",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "jomashop-logo.jpg",
+      imgSrc: "jomashop-background.jpg",
+      infoText: "Jomashop",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "adidas-logo.jpg",
+      imgSrc: "adidas-background.jpg",
+      infoText: "Adidas",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "nike-logo.jpg",
+      imgSrc: "nike-background.jpg",
+      infoText: "Nike",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "macys-logo.jpg",
+      imgSrc: "macys-background.jpg",
+      infoText: "Macys",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "coachoutlet-logo.jpg",
+      imgSrc: "coachoutlet-background.jpg",
+      infoText: "Coachoutlet",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "asos-logo.jpg",
+      imgSrc: "asos-background.jpg",
+      infoText: "Asos",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "farfetch-logo.jpg",
+      imgSrc: "farfetch-background.jpg",
+      infoText: "Farfetch",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "gap-logo.jpg",
+      imgSrc: "gap-background.jpg",
+      infoText: "Gap",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "iherb-logo.jpg",
+      imgSrc: "iherb-background.jpg",
+      infoText: "IHerb",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "sephora-logo.jpg",
+      imgSrc: "sephora-background.jpg",
+      infoText: "Sephora",
+      href: "https://www.google.com/"
+    },    
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "fashionNova-logo.jpg",
+      imgSrc: "fashionNova-background.jpg",
+      infoText: "FashionNova",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "pullandbear-logo.jpg",
+      imgSrc: "pullandbear-background.jpg",
+      infoText: "Pullandbear",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "target-logo.jpg",
+      imgSrc: "target-background.jpg",
+      infoText: "Target",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "corlourpop-logo.jpg",
+      imgSrc: "corlourpop-background.jpg",
+      infoText: "Corlourpop",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "nordstrom-logo.jpg",
+      imgSrc: "nordstrom-background.jpg",
+      infoText: "Nordstrom",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "cider-logo.jpg",
+      imgSrc: "cider-background.jpg",
+      infoText: "Cider",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "zappos-logo.jpg",
+      imgSrc: "zappos-background.jpg",
+      infoText: "Zappos",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "tennis-logo.jpg",
+      imgSrc: "tennis-background.jpg",
+      infoText: "Tennis Express",
       href: "https://www.google.com/"
     },
     {
       saleText: "UP TO 75% OFF",
-      logoSrc: "logo.jpg",
-      imgSrc: "banner.jpg",
-      infoText: "Store 1",
+      logoSrc: "massimodutti-logo.jpg",
+      imgSrc: "massimodutti-background.jpg",
+      infoText: "Massimodutti",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "kiehls-logo.jpg",
+      imgSrc: "kiehls-background.jpg",
+      infoText: "Kiehls",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "newera-logo.jpg",
+      imgSrc: "newera-background.jpg",
+      infoText: "New Era Cap",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "gymshark-logo.jpg",
+      imgSrc: "gymshark-background.jpg",
+      infoText: "Gymshark US",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "kith-logo.jpg",
+      imgSrc: "kith-background.jpg",
+      infoText: "Kith",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "stussy-logo.jpg",
+      imgSrc: "stussy-background.jpg",
+      infoText: "Stüssy",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "samsclub-logo.jpg",
+      imgSrc: "samsclub-background.jpg",
+      infoText: "Samsclub",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "spigen-logo.jpg",
+      imgSrc: "spigen-background.jpg",
+      infoText: "Spigen Inc",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "KICKSCREW-logo.jpg",
+      imgSrc: "KICKSCREW-background.jpg",
+      infoText: "KICKSCREW",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "decathlon-logo.jpg",
+      imgSrc: "decathlon-background.jpg",
+      infoText: "Decathlon",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "kyliecosmetics-logo.jpg",
+      imgSrc: "kyliecosmetics-background.jpg",
+      infoText: "Kyliecosmetics",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "HYDROGEN-logo.jpg",
+      imgSrc: "HYDROGEN-background.jpg",
+      infoText: "HYDROGEN",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "sokoglam-logo.jpg",
+      imgSrc: "sokoglam-background.jpg",
+      infoText: "Sokoglam.Com",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "amazon-logo.jpg",
+      imgSrc: "amazon-background.jpg",
+      infoText: "Amazon",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "tennisw-logo.jpg",
+      imgSrc: "tennisw-background.jpg",
+      infoText: "Tennis Warehouse",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "rainier-logo.jpg",
+      imgSrc: "rainier-background.jpg",
+      infoText: "Rainier Watch",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "NCASE-logo.jpg",
+      imgSrc: "NCASE-background.jpg",
+      infoText: "NCASE",
+      href: "https://www.google.com/"
+    },    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "trandingfoe-logo.jpg",
+      imgSrc: "trandingfoe-background.jpg",
+      infoText: "Store Tradingfoe",
+      href: "https://www.google.com/"
+    },
+    {
+      saleText: "UP TO 75% OFF",
+      logoSrc: "gentlemonster-logo.jpg",
+      imgSrc: "gentlemonster-background.jpg",
+      infoText: "Gentlemonster",
       href: "https://www.google.com/"
     },
   ],
